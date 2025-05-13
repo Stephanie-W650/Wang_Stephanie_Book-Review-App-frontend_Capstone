8796ca32af5a113b3bf13a365776ef8c485d7b53
Stephanie W <liw.w235@gmail.com>
2025-05-13 01:14:27 -0400

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+        
+    </>
+  )
+}
+
+export default App
